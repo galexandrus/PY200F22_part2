bb7b7139e2444abb26a502f3fd250e2c98d0a658
galexandrus <gavrilchuk_a@mail.ru>
2023-02-08 00:47:40 +0300

    topic 4 lab 1: testing, add hero, modify turbine

--- Занятие 4. Тестирование/Лабораторные задания/task1/main.py
+++ Занятие 4. Тестирование/Лабораторные задания/task1/main.py
@@ -1,6 +1,38 @@
-from turbine import Turbine
+from turbine import SteamTurbine
+from hero import Hero, NormalHero, StraightHero
 
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    T150 = SteamTurbine(150, 127.5, "Т", 18)
+    print(T150)
+    print(repr(T150))
+    T150.pk = 12
+    R100 = SteamTurbine(100, 127.5, "Р", 3)
+    print(R100)
+    print(repr(R100))
+
+    print("")
+
+    print(Hero.hero_count)
+    Simpleton = Hero("Simpleton")
+    print(Simpleton)
+    print(repr(Simpleton))
+    print(Hero.hero_count)
+
+    print(NormalHero.hero_count)
+    John = NormalHero("John")
+    print(John)
+    print(repr(John))
+    John.phrase = "мы идём по кругу"
+    print(John.phrase)
+    print(NormalHero.hero_count)
+    Ben = NormalHero("Ben", "я тоже нормальный")
+    print(Ben.phrase)
+    print(NormalHero.hero_count)
+
+    Boris = StraightHero("Boris")
+    print(Boris)
+    print(repr(Boris))
+    Boris.phrase = "пришло время свернуть"
+    print(Boris.phrase)
+    print(Hero.hero_count)
--- Занятие 4. Тестирование/Лабораторные задания/task1/turbine.py
+++ Занятие 4. Тестирование/Лабораторные задания/task1/turbine.py
@@ -1,5 +1,5 @@
+from abc import abstractmethod
 from typing import Union
-from abc import ABC, abstractmethod
 
 
 class Turbine:
@@ -7,56 +7,87 @@ class Turbine:
     def __init__(self, power: Union[int, float], pressure: Union[int, float]):
         """
         Конструктор турбины
-        :param power: мощность турбины
-        :param pressure: давление среды на входе в турбину
+        :param power: мощность турбины, МВт
+        :param pressure: давление среды на входе в турбину, МПа
         """
         self.check_power(power)
-        self.power = power
         self.check_pressure(pressure)
-        self.pressure = pressure
+        self._power = power
+        self._pressure = pressure
 
+    @abstractmethod
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.power}, {self.pressure})"
+        pass
 
     def __str__(self) -> str:
-        return str(f"Турбина мощностью {self.power} с давлением на входе {self.pressure}")
+        return str(f"Турбина мощностью {self.power} МВт с давлением на входе {self.pressure} МПа")
+
+    @property
+    def power(self) -> Union[int, float]:
+        return self._power
+
+    @property
+    def pressure(self) -> Union[int, float]:
+        return self._pressure
 
     @staticmethod
     def check_power(power: Union[int, float]):
         """
         Проверка значения мощности
-        :param power: мощность
+        :param power: мощность, МВт
         """
         if not (isinstance(power, int) or isinstance(power, float)):
-            raise TypeError("мощность должны быть int или float")
+            raise TypeError("мощность должна быть числом")
 
     @staticmethod
     def check_pressure(pressure: Union[int, float]):
         """
         Проверка значения давления
-        :param pressure: давление
+        :param pressure: давление, МПа
         """
         if not (isinstance(pressure, int) or isinstance(pressure, float)):
-            raise TypeError("давление должно быть float")
+            raise TypeError("давление должно быть числом")
 
 
 class SteamTurbine(Turbine):
-    def __init__(self, power: Union[int, float], pressure: Union[int, float], type_: str):
+    def __init__(self, power: Union[int, float], pressure: Union[int, float], type_: str, pk: Union[int, float]):
         """
         Конструктор паровой турбины
-        :param power: мощность
-        :param pressure: давление
+        :param power: мощность, МВт
+        :param pressure: давление, МПа
+        :param pk: давление в конденсаторе, кПа (для турбин типа "Р" бар)
         :param type_: тип
         """
-        self.check_power(power)
-        self.power = power
-        self.check_pressure(pressure)
-        self.pressure = pressure
+        super().__init__(power, pressure)
         self.check_type(type_)
-        self.type = type_
+        self._type = type_
+        self.check_pk(pk)
+        self.pk = pk
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self._power}, {self._pressure}, "{self._type}", {self._pk})'
+
+    @property
+    def type_(self) -> str:
+        return self._type
+
+    @property
+    def pk(self) -> Union[int, float]:
+        return self.pk
+
+    @pk.setter
+    def pk(self, new_pk):
+        self.check_pk(new_pk)
+        self._pk = new_pk
 
     @staticmethod
     def check_type(type_: str):
         types = ["П", "Т", "ПТ", "Р"]
         if type_ not in types:
-            raise ValueError("возможные типы паровых турбин: П, Т, ПТ, Р")
+            raise ValueError("здесь можно использовать следующие типы паровых турбин: П, Т, ПТ, Р")
+
+    def check_pk(self, pk: Union[int, float]):
+        if not (isinstance(pk, int) or isinstance(pk, float)):
+            raise TypeError("давление в конденсаторе должно быть числом")
+        if self._type != "Р" and pk > 20:
+            raise ValueError("слишком высокое давление в конденсаторе")
